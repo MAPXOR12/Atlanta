c07b699311558def24d4fd8c276b3701e83b6ab0
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-26 15:14:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
 	/* The token of your Discord Bot */
-	token: "Nzk0OTYxMTgwMDU3NDAzNDMz.X_CbSg.QymMc_kq3n_Ijsx0FwvGA5yYFhA",
+	token: "Nzk0OTYxMTgwMDU3NDAzNDMz.X_CbSg.bxwM63zVpVXYCxj_8jNi7RAPO04",
 	/* For the support server */
 	support: {
-		id: "XXXXXXXXXXX", // The ID of the support server
-		logs: "XXXXXXXXXXX", // And the ID of the logs channel of your server (new servers for example)
+		id: "", // The ID of the support server
+		logs: "", // And the ID of the logs channel of your server (new servers for example)
 	},
 	/* Dashboard configuration */
 	dashboard: {
 		enabled: false, // whether the dashboard is enabled or not
-		secret: "XXXXXXXXXXX", // Your discord client secret
+		secret: "", // Your discord client secret
 		baseURL: "https://dashboard.atlanta-bot.fr", // The base URl of the dashboard
-		logs: "XXXXXXXXXXX", // The channel ID of logs
+		logs: "", // The channel ID of logs
 		port: 8080, // Dashboard port
 		expressSessionPassword: "XXXXXXXXXXX", // Express session password (it can be what you want)
 		failureURL: "https://www.atlanta-bot.fr" // url on which users will be redirected if they click the cancel button (discord authentication)
@@ -25,29 +25,29 @@ module.exports = {
 	},
 	/* Bot's owner informations */
 	owner: {
-		id: "XXXXXXXXXXX", // The ID of the bot's owner
+		id: "", // The ID of the bot's owner
 		name: "Androz#2091" // And the name of the bot's owner
 	},
 	/* DBL votes webhook (optional) */
 	votes: {
 		port: 5000, // The port for the server
-		password: "XXXXXXXXXXX", // The webhook auth that you have defined on discordbots.org
-		channel: "XXXXXXXXXXX" // The ID of the channel that in you want the votes logs
+		password: "", // The webhook auth that you have defined on discordbots.org
+		channel: "" // The ID of the channel that in you want the votes logs
 	},
 	/* The API keys that are required for certain commands */
 	apiKeys: {
 		// BLAGUE.XYZ: https://blague.xyz/
-		blagueXYZ: "XXXXXXXXXXX",
+		blagueXYZ: "",
 		// FORTNITE TRN: https://fortnitetracker.com/site-api
 		fortniteTRN: "XXXXXXXXXXX",
 		// FORTNITE FNBR: https://fnbr.co/api/docs
 		fortniteFNBR: "XXXXXXXXXXX",
 		// DBL: https://discordbots.org/api/docs#mybots
-		dbl: "XXXXXXXXXXX",
+		dbl: "",
 		// AMETHYSTE: https://api.amethyste.moe
-		amethyste: "XXXXXXXXXXX",
+		amethyste: "",
 		// SENTRY: https://sentry.io (this is not required and not recommended - you can delete the field)
-		sentryDSN: "XXXXXXXXXXX"
+		sentryDSN: ""
 	},
 	/* The others utils links */
 	others: {
